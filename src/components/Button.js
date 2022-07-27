cb7fd139ed8a6e51d72a76ad1714ef093e7f7b4f
felipe-souza17 <felipesantos.rsouza@gmail.com>
2022-07-27 15:30:54 -0300

    Alterado Layout dos botões

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,17 +7,35 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').width / 4,
     width: Dimensions.get('window').width / 4,
     padding: 20,
-    backgroundColor: '#F0F0F0',
+    backgroundColor: '#000000',
+    color: '#fff',
     textAlign: 'center',
     borderWidth: 1,
-    borderColor: '#888'
+    borderColor: '#171717'
+  },
+  operationButton: {
+    backgroundColor: '#111111'
+  },
+  equalButton: {
+    backgroundColor: '#3E88D0'
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3
   }
 })
 
 export default props => {
+  const stylesButton = [styles.button]
+  if (props.double) stylesButton.push(styles.buttonDouble)
+  if (props.triple) stylesButton.push(styles.buttonTriple)
+  if (props.equal) stylesButton.push(styles.equalButton)
+  if (props.operation) stylesButton.push(styles.operationButton)
   return (
-    <TouchableHighlight onPress={props.onClick}>
-      <Text style={styles.button}>{props.label}</Text>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   )
 }
